b49a40a91c03692d21598a1504243e118a75f631
pjadda <jpavanaryan@gmail.com>
2018-04-04 18:27:25 -0400

    MessageChannel sysntax working, not custom interface

--- src/main/java/com/kafkastreams/messaging/GreetingsSender.java
+++ src/main/java/com/kafkastreams/messaging/GreetingsSender.java
@@ -1,18 +1,22 @@
 package com.kafkastreams.messaging;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
 @Service
+@EnableBinding(Source.class)
 public class GreetingsSender
 {
     @Autowired
-    private GreetingsStreams greetingsStreams;
+	private MessageChannel output; 
 
     public void send(Greetings greetings)
     {
-        greetingsStreams.outgoingGreetings().send(MessageBuilder.withPayload(greetings).build());
+        output.send(MessageBuilder.withPayload(greetings).build());
 
     }
 
