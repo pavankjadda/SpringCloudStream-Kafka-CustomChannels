769e8091e9250468b0eb69876457a4e6762c4a84
pjadda <jpavanaryan@gmail.com>
2018-04-04 23:30:04 -0400

    Bugs fixed. Deserialization not working. Will fix in future updates

--- src/main/java/com/kafkastreams/service/GreetingsSender.java
+++ src/main/java/com/kafkastreams/service/GreetingsSender.java
@@ -5,6 +5,7 @@ import com.kafkastreams.stream.GreetingsStreams;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.support.MessageBuilder;
@@ -13,12 +14,17 @@ import org.springframework.util.MimeTypeUtils;
 
 @Service
 @Slf4j
+@EnableAutoConfiguration
 public class GreetingsSender
 {
-    @Autowired
-	private  GreetingsStreams  greetingsStreams;
+    //@Autowired
+	//private  GreetingsStreams  greetingsStreams;
 
+    private final GreetingsStreams greetingsStreams;
 
+    public GreetingsSender(GreetingsStreams greetingsStreams) {
+        this.greetingsStreams = greetingsStreams;
+    }
     public void send(Greetings greetings)
     {
         MessageChannel messageChannel = greetingsStreams.outgoingGreetings();
